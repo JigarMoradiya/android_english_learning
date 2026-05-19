d0d4eeafc5fb48c28897d79c2d25e569f1b88590
JigarMoradiya <jigar2596moradiya@gmail.com>
2026-05-19 10:15:06 +0530

    policy url and change and release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
             resValue("string","app_name", "English Learning")
             dimension = "variant"
             applicationId = "com.vedaavi.english.learning"
-            versionCode = 6
+            versionCode = 7
             versionName = "1.0.2"
         }
     }
--- app/src/main/java/com/example/myapplication/utils/AppConstants.kt
+++ app/src/main/java/com/example/myapplication/utils/AppConstants.kt
@@ -4,6 +4,6 @@ package com.example.myapplication.utils
 object AppConstants {
     internal const val PREF_NAME = "vedaaviEnglish"
     internal const val ONESIGNAL_KEY = "5329e724-81b7-4a80-b942-3f0f1bcefa04"
-    internal const val PRIVACY_POLICY = "https://docs.google.com/document/d/15sM_PCeDNO1dYtQiaDa-cTCfXGc8FYwSZCo79su-B6A/edit?usp=drivesdk"
+    internal const val PRIVACY_POLICY = "https://drive.google.com/file/d/10FmFcb8obmx-bd2o3bM7TfBowTpjkeZH/view?usp=sharing"
     const val clickDisableTime = 1000L
 }
